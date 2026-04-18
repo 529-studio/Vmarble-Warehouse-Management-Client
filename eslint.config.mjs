e8aeb1d14a34ed3b5383c08d3c895b3659074e66
thdat-vu <vuthanhdat181@gmail.com>
2026-04-18 18:30:38 +0700

    fix: remove outdated next lint command [DatVT]

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,12 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
+import storybook from 'eslint-plugin-storybook'
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  ...storybook.configs["flat/recommended"]
+  ...nextCoreWebVitals,
+  ...storybook.configs['flat/recommended'],
+  {
+    ignores: ['storybook-static/**'],
+  },
 ]
 
 export default eslintConfig
